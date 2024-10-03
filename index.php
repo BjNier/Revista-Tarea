2f9f573cd92c50a604814279b09ffe0a8b7f967e
BjNier <benjaminjmr703@gmail.com>
2024-10-02 19:29:35 -0500

    Primer avance del trabajo de la Revista

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./resources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./resources/css/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="./resources/css/main.css">
+<body class="px-3">
+<div class="row mt-5">
+        <div class="col-sm-12 col-md-6" > 
+            <h1 class="text-center">EN LINEA</h1>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <p class="text-right pr-5">
+                SOLUCIONES MUNDIALES
+            </p>
+        </div>
+    </div>
+    <!-- Fin del encabezado -->
+     <div class="row">
+        <div class="col-sm-12">
+            <img class="w-100" style="height: 350px;" src="./resources/img/baner01.PNG" alt="">
+        </div>
+     </div>
+     <!-- Fin Cuerpo -->
+      <div class="container mt-4 border">
+            <h1 class="h1">SOLUCIONES CREATIVAS</h1>
+            <p>Sin importar cual sea tu negocio, nuesttro equipo puede hacerlo desde cero.</p>
+            <div class="row">
+            <div class="col-sm-12 col-md-4 border">
+                <div class="card boder-0">
+                    <div class="card-body">
+                        <img class="w-100" src="./resources/img/idea_01.PNG" alt="">
+                        <h5 class="h5 mt-2">
+                        <i class="bi bi-pencil-fill"></i>Diseño</h5>  
+                        <p class="pt-0">Creamos diseños segun tus nesecidades</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 border">
+                <div class="card">
+                    <div class="card-body">
+                        <img class="w-100" src="./resources/img/idea_02.PNG" alt="">
+                        <h5 class="h5 mt-2">
+                        <i class="bi bi-flower1"></i>Desarrollo</h5>  
+                        <p class="pt-0">Nuestros Programadores te garantizan la perfección</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 border ">
+                <div class="card">
+                    <div class="card-body">
+                        <img class="w-100" src="./resources/img/idea_03.PNG" alt="">
+                        <h5 class="h5 mt-2">
+                        <i class="bi bi-pencil-fill"></i>EDICIÓN DE LIBROS</h5>  
+                        <p class="pt-0">Que el producto final llegue a tus clientes</p>
+                    </div>
+                </div>
+            </div>
+       </div>
+      </div>
+      <!-- Hacemos el Pie -->
+       <footer class="bg-wine">
+            <p class="p-5 text-white">Edificio IDEX PJ, jr. Amazonas 120- Chachapoyas Chachapoyas-Amazonas</p>
+       </footer>
+</body>
+</html>
